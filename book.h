83d728da9a69cd454cefde2d839188418d5f870e
daiksoul <daiksoul@gmail.com>
2020-05-03 21:00:55 +0900

    chooseby update

--- book.h
+++ book.h
@@ -3,6 +3,22 @@
 
 typedef struct{
 	char name[20];
+	char genre[20];
 	char author[20];
+	int price;
 	int star;
 }Book;
+
+int addbook(Book *b);
+void readbook(Book b);
+int updatebook(Book *b);
+int deleteproduct(Book *b);
+int selectMenu();
+void listbook(Book *b, int count);
+int selectdatano(Book *b, int count);
+void savebook(Book *b, int count);
+int loadbook(Book *b);
+void searchbyname(Book *b, int count);
+void searchbyauthor(Book *b, int count);
+void searchbystar(Book *b, int count);
+void chooseby(Book *b, int count);
