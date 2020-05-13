93aff6c163ac638c341558e37890819fc1d587a8
daiksoul <daiksoul@gmail.com>
2020-05-13 21:20:12 +0900

    Update search.c

--- search.c
+++ search.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "book.h"
 
 void searchbyname(Book *b, int count){
 	char keyw[20];
 	printf("키워드를 입력하세요 : ");
-	scanf("%s",keyw);
+	scanf("%[^\n]",keyw);
 	for(int i =0 ; i<count; i++)
 		if(strset(b[i].name,keyw))
 			readbook(b[i]);
@@ -13,15 +14,17 @@ void searchbyname(Book *b, int count){
 void searchbyauthor(Book *b, int count){
 	char keyw[20];
 	printf("키워드를 입력하세요 : ");
-	scanf("%s",keyw);
+	scanf("%[^\n]",keyw);
 	for(int i =0 ; i<count; i++)
 		if(strset(b[i].author,keyw))
 			readbook(b[i]);
 }
 
 void chooseby(Book *b, int count){
+	int sel = 0;
 	printf("검색 방법을 선택해주세요. \n(0:취소 1:책 제목 2:작가명 3:별점): ");
-	int sel = scanf("%d",&sel);
+	
+	scanf("%d",&sel);
 	
 	switch (sel){
 	
